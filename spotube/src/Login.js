962ff30d4153f831086048be308e2df1705bb25e
Dhruv132k <2013dhruv@gmail.com>
2024-07-20 20:17:40 +0530

    Initial commit

---
+++ spotube/src/Login.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import spotubelogo from './images/Spotify_Primary_Logo_RGB_Black.png';
+import './Login.css'
+
+function Login() {
+    return (
+        <div className='login'>
+            {/* This will be the main page having the logo and login button */}
+            <img
+                src={spotubelogo}
+                alt=''
+            />
+            <a>LOGIN WITH SPOTIFY</a> {/*I am using a link because upon clicking it users will be redirected to spotify service to login */}
+        </div>
+    )
+}
+
+export default Login
---
+++ spotube/src/spotify.js
@@ -0,0 +1,7 @@
+// All the spotify logic not a component
+
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+const redirectUri = "http://localhost:3000/"
+
+const clientId = "99625eb7a04548adbc485dba62904a76"
